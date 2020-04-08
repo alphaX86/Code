293585c7e6a7c041fd875c0e9cc5bdec628fe7aa
Aadhitya A <59508546+echo-864@users.noreply.github.com>
2020-04-08 20:38:02 +0530

    Create string.cpp

---
+++ string.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+  string s,r;
+  cin>>s;
+  cin.ignore();
+  getline(cin,r);
+  cout<<s<<endl;
+  cout<<r<<endl;
+  return 0;
+}
