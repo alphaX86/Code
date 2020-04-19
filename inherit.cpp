105d84874bc515cb5aa2846e92d4f73bb2827457
Aadhitya A <59508546+echo-864@users.noreply.github.com>
2020-04-19 19:45:19 +0530

    Create inherit.cpp

---
+++ inherit.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Person{
+	protected:
+		string firstName;
+		string lastName;
+		int id;
+	public:
+		Person(string firstName, string lastName, int identification){
+			this->firstName = firstName;
+			this->lastName = lastName;
+			this->id = identification;
+		}
+		void printPerson(){
+			cout<< "Name: "<< lastName << ", "<< firstName <<"\nID: "<< id << "\n"; 
+		}
+	
+};
+
+class Student :  public Person{
+	private:
+		vector<int> testScores; 
+        double avg = 0.0;
+        int subs = 0; 
+        int sum = 0;
+	public:
+        /*	
+        *   Class Constructor
+        *   
+        *   Parameters:
+        *   firstName - A string denoting the Person's first name.
+        *   lastName - A string denoting the Person's last name.
+        *   id - An integer denoting the Person's ID number.
+        *   scores - An array of integers denoting the Person's test scores.
+        */
+        // Write your constructor here
+        Student(string fN, string lN, int id, vector<int> scores) : Person(fN, lN, id)
+        {
+            this->testScores=scores;
+            subs=testScores.size();    
+        }
+        /*	
+        *   Function Name: calculate
+        *   Return: A character denoting the grade.
+        */
+        // Write your function here
+        char calculate()
+        {
+            char s;
+            for(int i=0;i<subs;i++){
+                sum += testScores[i];
+            }
+            avg=sum/subs;
+            if(avg<=100 && avg>=90){
+                s = 'O';
+            }
+            else if(avg<90 && avg>=80){
+                s = 'E';
+            }
+            else if(avg<80 && avg>=70){
+                s = 'A';
+            }
+            else if(avg<70 && avg>=55){
+                s = 'P';
+            }
+            else if(avg<55 && avg>=40){
+                s = 'D';
+            }
+            else{
+                s = 'T';
+            }
+            return s;
+        }
+
+};
+
+int main() {
+	string firstName;
+  	string lastName;
+	int id;
+  	int numScores;
+	cin >> firstName >> lastName >> id >> numScores;
+  	vector<int> scores;
+  	for(int i = 0; i < numScores; i++){
+	  	int tmpScore;
+	  	cin >> tmpScore;
+		scores.push_back(tmpScore);
+	}
+	Student* s = new Student(firstName, lastName, id, scores);
+	s->printPerson();
+	cout << "Grade: " << s->calculate() << "\n";
+	return 0;
+}
