67f1b15e0f6993705242dfc96f399e2fc3d72539
Aadhitya A <59508546+echo-864@users.noreply.github.com>
2020-05-25 20:44:46 +0530

    Create log in java file

---
+++ log.java
@@ -0,0 +1,7 @@
+public class log {
+  public staic void main(String[] args){
+    //This is a log of my activites xD
+    System.out.println("Applied for Twitter Dev account at May 25 2020");
+    System.out.println("I need to wait for a better hardware and account approval");
+    }
+}
